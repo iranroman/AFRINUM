04e530f55448f211ac9585023bb791c634f6e378
iranroman <iran@ccrma.stanford.edu>
2022-05-08 18:58:40 -0400

    updates analysis

--- pitch_analyze.py
+++ pitch_analyze.py
@@ -1,6 +1,12 @@
+from scipy.stats import linregress
+import os
 import matplotlib.pyplot as plt
+from matplotlib import colors
 import numpy as np
 import math
+import scipy.stats as stats
+import librosa
+import librosa.display
 
 def get_freq_deltas(pitches):
 
@@ -17,33 +23,74 @@ def get_freq_deltas(pitches):
 
 
 if __name__ == "__main__":
-    #read in frequencies
-    g_freq = np.genfromtxt('ghana_flute_sample_f0.csv',delimiter=',')[1:,1]
-    w_freq = np.genfromtxt('western_flute_sample_f0.csv',delimiter=',')[1:,1]
-    
-    g_deltas = get_freq_deltas(g_freq)
-    w_deltas = get_freq_deltas(w_freq)
 
-    plt.hist(g_deltas, bins=50, label='Ghana', fc=(0,0,1,0.5))
-    plt.hist(w_deltas, bins=50, label='western', fc=(1,0,0,0.5))
-    plt.xlabel('cent delta')
-    plt.legend()
-    plt.savefig('delta_histogram.png')
+    all_f0_files = os.listdir('Selected_Daddy_Lumba_Songs/vocals/crepe_output/')
+    all_f0_files.sort()
+    means = []
+    stds = []
+    years = []
+    hists = []
+    for f0s in all_f0_files:
+        #x, fs = librosa.load('Selected_Daddy_Lumba_Songs/'+f0s.split('.')[0]+'.mp3', sr=8000)
+        #X = librosa.feature.melspectrogram(x, sr=fs, n_fft=2014,win_length=256,hop_length=int(fs/100), )
+        #S_db = librosa.amplitude_to_db(np.abs(X), ref=np.max)
+
+        #plt.figure(figsize=(30,10))
+        #librosa.display.specshow(S_db[:,:1000], sr=fs, hop_length=int(fs/100), x_axis='time', y_axis='mel')
+        #plt.savefig('Selected_Daddy_Lumba_Songs/vocals/plots/year_{}_spec.png'.format(f0s[:4]))
+        #plt.close()
+
+        #read in frequencies
+        g_freq = np.genfromtxt('Selected_Daddy_Lumba_Songs/vocals/crepe_output/'+f0s,delimiter=',')[:,1]
+        conf = np.genfromtxt('Selected_Daddy_Lumba_Songs/vocals/crepe_output/'+f0s,delimiter=',')[:,2]
+        
+        #print(X.shape)
+        #print(g_freq.shape)
+        #input()
+
+        #g_freq = [f for f,c in zip(g_freq,conf) if f<300 and f>80 and c > 0.9]
+        g_freq = [(f,i) for i, (f,c) in enumerate(zip(g_freq,conf)) if f<600 and f>150 and c > 0.9]
+        g_freq, time = list(map(list, zip(*g_freq)))
+
+        g_deltas = get_freq_deltas(g_freq)
+        g_cents = np.array([(1200)*np.log2(i/16.352) for i in g_freq])
+        hists.append(np.histogram(g_cents,bins=range(3800,6300), density=True)[0])
+
+        plt.hist(g_deltas, bins=50, fc=(0,0,1,0.5), density=True)
+        plt.xlabel('cent delta')
+        plt.savefig('Selected_Daddy_Lumba_Songs/vocals/plots/year_{}.png'.format(f0s[:4]))
+        plt.close()
+
+        means.append(np.mean(g_deltas))
+        stds.append(np.std(g_deltas))
+        years.append(int(f0s[:4]))
+
+    plt.figure(figsize=(10,10))
+    edges = np.arange(3800,6400,100)
+    hists = np.array(hists/np.max(hists,axis=1,keepdims=True))
+    plt.imshow(hists.T, aspect='auto', origin='lower', extent=[0, len(years), edges[0], edges[-1]])
+    plt.yticks(edges,['D','','E','F','','G','','A','','B','C','','D','','E','F','','G','','A','','B','C','','D',''])
+    plt.xticks(range(len(years)),[str(y) for y in years],rotation=45)
+    plt.grid()
+    plt.savefig('Selected_Daddy_Lumba_Songs/vocals/plots/time_hists.png')
     plt.close()
 
-    plt.subplot(2,1,1)
-    t = np.arange(0,len(g_freq)/100,0.01)
-    plt.plot(t,g_freq, label='Ghana')
-    plt.plot(t,w_freq, label='western')
-    plt.xlabel('time (s)')
-    plt.ylabel('Hertz')
+    years = np.array(years)
+    res_means = linregress(years, means)
+    res_stds = linregress(years, stds)
+    plt.scatter(years,means)
+    plt.plot(years, res_means.intercept + res_means.slope*years, 'r')
+    plt.xlabel('years')
+    plt.ylabel('mean difference from equal temperament')
     plt.grid()
-    plt.legend()
-    plt.subplot(2,1,2)
-    plt.plot(t,g_deltas, label='Ghana')
-    plt.plot(t,w_deltas, label='western')
-    plt.xlabel('time (s)')
-    plt.ylabel('Cents')
-    plt.legend()
+    plt.title('R = {:.4f}, p = {:.4f}'.format(res_means.rvalue, res_means.pvalue))
+    plt.savefig('Selected_Daddy_Lumba_Songs/vocals/plots/means_years.png')
+    plt.close()
+    plt.scatter(years,stds)
+    plt.plot(years, res_stds.intercept + res_stds.slope*years, 'r')
+    plt.xlabel('years')
+    plt.ylabel('standard deviation of the difference from equal temperament')
     plt.grid()
-    plt.savefig('timeseries.png')
+    plt.title('R = {:.4f}, p = {:.4f}'.format(res_stds.rvalue, res_stds.pvalue))
+    plt.savefig('Selected_Daddy_Lumba_Songs/vocals/plots/stds_years.png')
+    plt.close()
